c75363269216aa567fdde5538fdb23a1952c5e3a
stingaciutiberiu <tiberiu.stingaciu@sevio.com>
2020-10-04 15:51:10 +0300

    Tema1

---
+++ Tema1/Assets/AnimationCactus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCactus : MonoBehaviour
+{
+    public float distance;
+    public GameObject Cactus;
+    Animation CactusAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cactus = GameObject.FindGameObjectWithTag("Cactus");
+        CactusAnimation = Cactus.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(this.transform.position, Cactus.transform.position);
+        if(distance < 5)
+        {
+            CactusAnimation.wrapMode = WrapMode.Loop;
+            CactusAnimation.Play("Attack");
+        }
+        else
+        {
+            CactusAnimation.wrapMode = WrapMode.Loop;
+            CactusAnimation.Play("Idle");
+        }
+    }
+}
